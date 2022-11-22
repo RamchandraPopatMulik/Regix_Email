e9a7de3a9ca4c1f48efb96f840bf4436d4e9d116
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-22 12:36:21 +0530

    Added file

---
+++ RegexEmail_Validate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RegexEmail_Validate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Email: ");
+            string email = Console.ReadLine();
+
+            RegexProgram regex=new RegexProgram();
+            regex.CheckValidate(email);
+
+        }
+    }
+}
---
+++ RegexEmail_Validate/Regex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexEmail_Validate
+{
+    public class RegexProgram
+    {
+        public void CheckValidate(string Pattern)
+        {
+            Regex code = new Regex(@"(abc)");
+            bool matches=code.IsMatch(Pattern);
+            if(matches == true)
+            {
+                Console.WriteLine("Valide Email");
+            }
+            else
+            {
+                Console.WriteLine("InValide Email");
+            }
+        }
+    }
+}
